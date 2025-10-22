d0008b917eaee97763ee8c4fd2327ae2be3104e5
Eonfge <33983090+Eonfge@users.noreply.github.com>
2025-10-22 21:32:16 +0200

    Start-up wrapper (#117)
    Applies default settings

---
+++ resources/slade3.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euf -o pipefail
+
+CFG_FILE=~/.slade3/slade3.cfg
+
+if [ ! -f "$CFG_FILE" ]; then
+    install -Dm0644 /app/bin/slade3.cfg $CFG_FILE
+fi
+
+exec /app/bin/slade "$0"
