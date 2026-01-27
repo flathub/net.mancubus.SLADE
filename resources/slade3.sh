de5b57868abb40d371d762ca5653294701c454e6
Eonfge <33983090+Eonfge@users.noreply.github.com>
2026-01-27 10:08:26 +0000

    End-of-life

--- resources/slade3.sh
+++ resources/slade3.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euf -o pipefail
 
-zenity --info --text='This Flatpak is no longer supported.\nPlease, use the AppImage which is better supported.\n\n<a href="https://slade.mancubus.net/index.php?page=downloads">SLADE Downloads</a>' --width=400
+zenity --info --text='This Flatpak is no longer maintained.\nPlease, use the AppImage which is better supported.\n\n<a href="https://slade.mancubus.net/index.php?page=downloads">SLADE Downloads</a>' --width=400
